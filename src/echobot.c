4c54c518bebe6a44932aee1f6f928adb8fffcfc2
Impyy <alex@impy.me>
2016-04-15 00:03:16 +0200

    Actually fix the bug mentioned in the previous commit message this time

--- src/echobot.c
+++ src/echobot.c
@@ -33,7 +33,8 @@ void friend_cleanup(Tox *tox)
 	uint64_t curr_time = time(NULL);
 	for (uint32_t i = 0; i < friend_count; i++) {
 		TOX_ERR_FRIEND_GET_LAST_ONLINE err;
-		uint64_t last_online = tox_friend_get_last_online(tox, friends[i], &err);
+		uint32_t friend = friends[i];
+		uint64_t last_online = tox_friend_get_last_online(tox, friend, &err);
 
 		if (err != TOX_ERR_FRIEND_GET_LAST_ONLINE_OK) {
 			printf("couldn't obtain 'last online', this should never happen\n");
@@ -41,8 +42,8 @@ void friend_cleanup(Tox *tox)
 		}
 
 		if (curr_time - last_online > 2629743) {
-			printf("removing friend %d\n", i);
-			tox_friend_delete(tox, i, NULL);
+			printf("removing friend %d\n", friend);
+			tox_friend_delete(tox, friend, NULL);
 		}
 	}
 }
